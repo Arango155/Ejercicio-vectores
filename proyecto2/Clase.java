9c9781a6627706d5aeabc49e13dd6a0cf88ecf88
Arango155 <jdarango031@gmail.com>
2022-05-14 11:02:24 -0600

    Agrego los archivos de apache neatbeans para poder resolver el ejercicio numero 3 de la tarea de ejercicios con vectores

---
+++ proyecto2/Clase.java
@@ -0,0 +1,25 @@
+
+package proyecto2;
+
+public class Clase {
+
+  public Clase(){
+  
+  }
+  
+  
+  public void mostrarClase (){
+  
+  int numeroscien [] = new int [100];
+             for(int i=0; i<numeroscien.length; i++){
+       
+           numeroscien[i]=i+1;
+           System.out.println(numeroscien[i]);   
+           }
+             
+  }
+
+  
+    
+  
+}
---
+++ proyecto2/Proyecto2.java
@@ -0,0 +1,24 @@
+
+package proyecto2;
+
+import java.util.Scanner;
+
+
+public class Proyecto2 {
+
+    
+      public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        Clase proyecto2=new Clase();
+  
+      proyecto2.mostrarClase();
+             
+             
+    }
+    
+    
+         
+    
+}
